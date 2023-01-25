0a613c0bf5b6f066ab3bfb906584d2d3594a7c09
Ruari O'Hara <2666047o@student.gla.ac.uk>
2023-01-25 22:56:09 +0000

    chapter 6 complete failed 4 tests will come back in morning

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from rango.models import Category, Page
+
+class CategoryAdmin(admin.ModelAdmin):
+   prepopulated_fields = {'slug':('name',)}
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')

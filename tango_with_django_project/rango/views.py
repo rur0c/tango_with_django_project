a13ec209cb29210f95b74c49468f63754ccdb57d
Ruari O'Hara <2666047o@student.gla.ac.uk>
2023-01-30 13:20:05 +0000

    chapter 6 completed (1 error unsolved)

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,16 +4,13 @@ from rango.models import Category
 from rango.models import Page
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
-    try:
-        pages = Page.objects.order_by('-views')[:5]
-        context_dict['pages'] = pages
-    except Page.DoesNotExist:
-        context_dict['pages'] = None
-    return render(request, 'rango/index.html', context=context_dict)
+     context_dict = {}
+     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+     category_list = Category.objects.order_by('-likes')[:5]
+     page_list = Page.objects.order_by('-views')[:5]
+     context_dict['categories'] = category_list
+     context_dict['pages'] = page_list
+     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
     context_dict = {'boldmessage': "This tutorial has been put together by Ruari"}
